908abfc76c95cce5f9dc5db39e27d6d270478e05
ridering <djfgvjbgj@gmail.com>
2020-03-25 18:22:58 +0500

    adds folding of search result and rewrites button structure

--- __main__.py
+++ __main__.py
@@ -8,10 +8,14 @@ from math import cos, radians
 pygame.init()
 size = width, height = 700, 500
 screen = pygame.display.set_mode(size)
-FPS = 300
+FPS = 60
 
 SCALES = [-79.5, 21.8, 70.225, 80.112, 82.992, 84.15, 84.61, 84.85, 84.972, 85.027,
           85.055, 85.065, 85.07, 85.074, 85.077, 85.079, 85.08, 85.0805]
+clock = pygame.time.Clock()
+running = True
+pressed = False
+buttons = pygame.sprite.Group()
 
 
 class Map:
@@ -23,7 +27,7 @@ class Map:
         self.render()
 
     def render(self):
-        pygame.draw.line(screen, pygame.Color("red"), (0, 45), (700, 45), 5)
+        pygame.draw.line(screen, pygame.Color("red"), (0, 45), (width, 45), 5)
         self.image = pygame.image.load(
             io.BytesIO(load_map([str(self.center[0]), str(self.center[1])],
                                 self.scale,
@@ -38,37 +42,70 @@ class Map:
             self.pt = self.center.copy()
 
 
+class Button(pygame.sprite.Sprite):
+    def __init__(self, text_text, color, x, y, bg_color='white', angle=0, font_size=30):
+        super().__init__(buttons)
+
+        self.width, self.height = self.get_size(text_text, font_size)
+        if angle == 270:
+            self.rect = pygame.Rect(x, y, self.height, self.width)
+        else:
+            self.rect = pygame.Rect(x, y, self.width, self.height)
+        self.rect.x = x
+        self.rect.y = y
+
+        self.static = self.make_button(text_text, color, bg_color, angle, self.width, self.height)
+        self.active = self.make_button(text_text, color, 'yellow', angle, self.width, self.height)
+        self.image = self.static
+
+        self.text = text_text.strip()
+
+    def get_size(self, text_text, font_size):
+        self.font = pygame.font.Font('font.ttf', font_size)
+        return pygame.font.Font.size(self.font, text_text)
+
+    def make_button(self, text_text, color, bg_color, angle, but_width, but_height):
+        text = self.font.render("{}".format(text_text), 1, pygame.Color(color))
+        screen.fill(pygame.Color(bg_color))
+        screen.blit(text, (0, 0))
+        pygame.draw.rect(screen, pygame.color.Color(color), (0, 0, but_width, but_height), 1)
+        image = screen.subsurface((0, 0, but_width, but_height)).copy()
+        return pygame.transform.rotate(image, angle)
+
+
 cur_map = Map()
-
+scheme = Button('  Схема  ', 'red', width - 50, height - 390, angle=270)
+sputnik = Button('  Спутник  ', 'blue', width - 50, height - 265, angle=270)
+hybrid = Button('  Гибрид  ', 'green', width - 50, height - 130, angle=270)
+search = Button('  Искать  ', 'black', width - 90, 5)
+cancel = Button('  Сброс поискового результата  ', 'black', width - 287, 50)
 text_input = text_input.TextInput(font_family='font.ttf', font_size=30,
-                                  repeat_keys_initial_ms=300, repeat_keys_interval_ms=30)
-
-clock = pygame.time.Clock()
-running = True
-pygame.key.set_repeat(10)
-pressed = False
+                                  repeat_keys_initial_ms=400, repeat_keys_interval_ms=30)
 
 
-def make_labels(text_text, color, rotate=True):
-    font = pygame.font.Font('font.ttf', 30)
-    text = font.render("{}".format(text_text), 1, pygame.Color(color))
-    screen.fill(pygame.Color('white'))
-    screen.blit(text, (0, 0))
-    image = screen.subsurface((0, 0, 80, 30)).copy()
-    if rotate:
-        return pygame.transform.rotate(image, 270)
-    return image
+def make_action(button):
+    if button == 'Искать':
+        cur_map.search(text_input.get_text())
+        search.image = search.active
+    if button == 'Схема':
+        cur_map.map_type = 'map'
+        scheme.image = scheme.active
+    if button == 'Спутник':
+        cur_map.map_type = 'sat'
+        sputnik.image = sputnik.active
+    if button == 'Гибрид':
+        cur_map.map_type = 'sat,skl'
+        hybrid.image = hybrid.active
+    if button == 'Сброс поискового результата':
+        cancel.image = cancel.active
+        cur_map.pt = None
 
-
-scheme = make_labels('Схема', 'red')
-sputnik = make_labels('Спутник', 'blue')
-hybrid = make_labels('Гибрид', 'green')
-search = make_labels('Искать', 'black', rotate=False)
 
 while running:
-    pygame.key.set_repeat(10)
     clock.tick(FPS)
     events = pygame.event.get()
+
+    text_input.update(events)
     scale = 0.1 ** (cur_map.scale // 4) * (17 - cur_map.scale + 3)
     for event in events:
         if event.type == pygame.QUIT:
@@ -94,18 +131,11 @@ while running:
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == pygame.BUTTON_LEFT:
-                if event.pos[0] in range(610, 710) and event.pos[1] in range(0, 40):
-                    cur_map.search(text_input.get_text())
-                elif event.pos[0] in range(650, 680):
-                    if event.pos[1] in range(130, 210):
-                        cur_map.map_type = 'map'
-                    elif event.pos[1] in range(235, 315):
-                        cur_map.map_type = 'sat'
-                    elif event.pos[1] in range(370, 450):
-                        cur_map.map_type = 'sat,skl'
-                else:
-                    pressed = True
-                    start_pos = event.pos
+                for button in buttons:
+                    if button.rect.collidepoint(*event.pos):
+                        make_action(button.text)
+                pressed = True
+                start_pos = event.pos
             if event.button == pygame.BUTTON_WHEELUP:
                 if cur_map.scale < 17:
                     cur_map.scale += 1
@@ -121,6 +151,8 @@ while running:
 
         if event.type == pygame.MOUSEBUTTONUP:
             pressed = False
+            for button in buttons:
+                button.image = button.static
 
     if cur_map.center[0] > 180:
         cur_map.center[0] -= 360
@@ -136,19 +168,11 @@ while running:
 
     screen.fill(pygame.Color('white'))
     cur_map.render()
-    screen.blit(cur_map.image, (0, 50))
+    screen.blit(cur_map.image, (width - 700, 50))
 
-    screen.blit(scheme, (650, 110))
-    screen.blit(sputnik, (650, 235))
-    screen.blit(hybrid, (650, 370))
-
-    pygame.key.set_repeat(0)
-    text_input.update(events)
     screen.blit(text_input.get_surface(), (10, 10))
-
-    pygame.draw.rect(screen, pygame.color.Color('white'), (610, 0, 100, 40))
-    pygame.draw.rect(screen, pygame.color.Color('black'), (610, 0, 100, 40), 1)
-    screen.blit(search, (620, 5))
+    buttons.draw(screen)
+    buttons.update()
 
     pygame.display.update()
 
